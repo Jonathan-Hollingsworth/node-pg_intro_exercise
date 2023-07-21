c1576f2e762142af406bd23ae2ad009c04dae850
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-07-21 13:40:37 -0400

    Fixed Directory Error

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const ExpressError = require("./expressError")
+const ExpressError = require("../expressError")
 const db = require("../db")
 
 router.get('/', async function(req, res, next) {
