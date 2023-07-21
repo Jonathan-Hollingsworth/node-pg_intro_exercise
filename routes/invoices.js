37a9ddcd758bd8e56f76e137af244350ccacd7ae
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-07-21 13:43:29 -0400

    Removed Damaging Comma

--- routes/invoices.js
+++ routes/invoices.js
@@ -14,7 +14,7 @@ router.get('/', async function(req, res, next) {
 
 router.get('/:id', async function(req, res, next) {
     try {
-        const iResults = await db.query(`SELECT id, amt, paid, add_date, paid_date, FROM invoices
+        const iResults = await db.query(`SELECT id, amt, paid, add_date, paid_date FROM invoices
         WHERE id=$1`, [req.params.id])
         const iCompCodes = await db.query(`SELECT comp_code FROM invoices WHERE id=$1` [req.params.id])
         if(iResults.rows.length === 0){
